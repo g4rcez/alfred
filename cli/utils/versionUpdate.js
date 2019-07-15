60f3c83d50dd71c9849e9b58f66d20200a475ff6
Garcez <allan.f.garcez@gmail.com>
2019-07-15 19:38:03 -0300

    test

--- cli/utils/versionUpdate.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var semver_1 = __importDefault(require("semver"));
-var versionUpdate = function (version, upgrade) { return semver_1.default.inc(version, upgrade, { loose: false }); };
-exports.default = versionUpdate;
