8d468b160d8950bef9771c33e70a478d07998dbc
Garcez <allan.f.garcez@gmail.com>
2019-07-16 19:32:26 -0300

    test

--- src/utils/versionUpdate.ts
+++ src/utils/versionUpdate.ts
@@ -5,7 +5,7 @@ const versions: VersionUpgrade[] = ["patch", "minor", "major"];
 
 const versionUpdate = (version: string, upgrade: VersionUpgrade) => {
 	if (semver.valid(version)) {
-		const correct = versions.filter((x) => version === upgrade).length === 1 ? upgrade : "patch";
+		const correct = versions.includes(upgrade.toUpperCase() as VersionUpgrade) ? upgrade : "patch";
 		return semver.inc(version, correct as VersionUpgrade, { loose: false });
 	}
 	return "0.0.0";
