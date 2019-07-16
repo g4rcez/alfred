6a9a48d583b6bc9e32ea807e296a2920441728e0
Garcez <allan.f.garcez@gmail.com>
2019-07-16 19:31:19 -0300

    Correct upgrade version logic

--- src/utils/versionUpdate.ts
+++ src/utils/versionUpdate.ts
@@ -5,7 +5,7 @@ const versions: VersionUpgrade[] = ["patch", "minor", "major"];
 
 const versionUpdate = (version: string, upgrade: VersionUpgrade) => {
 	if (semver.valid(version)) {
-		const correct = versions.filter((x) => version === x).length === 1 ? version : "patch";
+		const correct = versions.filter((x) => version === upgrade).length === 1 ? upgrade : "patch";
 		return semver.inc(version, correct as VersionUpgrade, { loose: false });
 	}
 	return "0.0.0";
