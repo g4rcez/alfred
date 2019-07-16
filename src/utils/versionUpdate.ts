72713b47d7a098f3ad26a85eed203204502d5149
Garcez <allan.f.garcez@gmail.com>
2019-07-16 19:21:36 -0300

    test

--- src/utils/versionUpdate.ts
+++ src/utils/versionUpdate.ts
@@ -5,7 +5,7 @@ const versions = ["patch", "minor", "major"];
 const versionUpdate = (version: string, upgrade: VersionUpgrade) => {
 	const correct = versions.includes(version) ? version : "patch";
 	const v = semver.inc(correct, upgrade, { loose: false });
-	console.log("VERSION CORRECT V", correct, v);
+	console.log("VERSION CORRECT V", version, correct, v);
 	return v;
 };
 
