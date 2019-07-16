c67458818a1ebf2b18c513ce7b5ca66fb8e4a30e
Garcez <allan.f.garcez@gmail.com>
2019-07-16 19:25:51 -0300

    v

--- src/commands/Version.ts
+++ src/commands/Version.ts
@@ -3,13 +3,13 @@ import { getPackageJson, setPackageJson } from "../utils/files";
 import Git from "../utils/github";
 import colors from "../styles/colors";
 export default async function Version(args: any) {
-	const mode = args.update;
+	const mode = args.update || "patch";
 	const msg = args.msg;
 	try {
 		const e: any = await Git.countStash();
 		if (e[0]) {
 			const packageJson = JSON.parse(getPackageJson());
-			console.log("CURRENT VERSION", packageJson.version);
+			console.log("CURRENT VERSION", packageJson.version, "mode");
 			const newVersion = versionUpdate(packageJson.version, mode) as string;
 			setPackageJson(JSON.stringify({ ...packageJson, version: newVersion }, null, 4));
 			const tagVersion = `v${newVersion}`;
--- src/utils/versionUpdate.ts
+++ src/utils/versionUpdate.ts
@@ -3,7 +3,7 @@ import semver from "semver";
 export type VersionUpgrade = "patch" | "minor" | "major";
 const versions = ["patch", "minor", "major"];
 const versionUpdate = (version: string, upgrade: VersionUpgrade) => {
-	const correct = versions.includes(version) ? version : "patch";
+	const correct = versions.filter((x) => version === x).length === 1 ? version : "patch";
 	const v = semver.inc(correct, upgrade, { loose: false });
 	console.log("VERSION CORRECT V", version, correct, v);
 	return v;
