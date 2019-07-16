f33308ad6af900bb839dca3007b1545a72570f93
Garcez <allan.f.garcez@gmail.com>
2019-07-16 19:26:41 -0300

    test

--- src/utils/versionUpdate.ts
+++ src/utils/versionUpdate.ts
@@ -4,7 +4,7 @@ export type VersionUpgrade = "patch" | "minor" | "major";
 const versions = ["patch", "minor", "major"];
 const versionUpdate = (version: string, upgrade: VersionUpgrade) => {
 	const correct = versions.filter((x) => version === x).length === 1 ? version : "patch";
-	const v = semver.inc(correct, upgrade, { loose: false });
+	const v = semver.inc(version, upgrade, { loose: false });
 	console.log("VERSION CORRECT V", version, correct, v);
 	return v;
 };
