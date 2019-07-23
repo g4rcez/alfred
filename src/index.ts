274ca01e258b9f4a421e5a3d5d8041332bfb1950
Garcez <allan.f.garcez@gmail.com>
2019-07-23 19:56:17 -0300

    v3.0.3

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,25 @@
 import cli from "commander";
 import Version from "./commands/Version";
 import Tags from "./commands/Tags";
+
 const program = new cli.Command();
+
 program.version("0.0.1");
 program
 	.command("version")
 	.alias("v")
 	.description("Update version of package.json and git tag version")
-	.option("-u, --update <release>", "Get the new version based on patch | minor | major", "patch")
+	.option(
+		"-u, --update <release>",
+		"Get the new version based on patch | minor | major",
+		"patch"
+	)
 	.option("-m, --msg <message>", "Message on commit")
-	.option("-n, --nolastcommit <boolean>", "Commit with last commit message", false)
+	.option(
+		"-n, --nolastcommit <boolean>",
+		"Commit with last commit message",
+		false
+	)
 	.action(Version);
 
 program
